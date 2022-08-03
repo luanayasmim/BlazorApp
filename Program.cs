4d589017cdb27fce9f88d2b3cd213c4b0180cefa
Luana Yasmim Lourenço <lylourenco@sercom.com.br>
2022-08-03 10:48:12 -0300

    Calculando o Imc com o razorPage

---
+++ Program.cs
@@ -0,0 +1,32 @@
+/*
+    Esse é o arquivo de entrada da aplicação, 
+    responsável por iniciar o servidor e configurar os serviços necessários.
+*/
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
